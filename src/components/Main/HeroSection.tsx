e415d7778299f965147d4b14f44649f0421e2e68
Michał Pawłowski <kontakt@michalpawlowski.pl>
2026-02-21 17:13:52 +0100

    poprawiono style HereSection

--- src/components/Main/HeroSection.tsx
+++ src/components/Main/HeroSection.tsx
@@ -3,18 +3,20 @@ import Button from "./Button";
 
 const Hero: React.FC = () => {
   return (
-    <section className="w-full flex flex-col md:flex-row items-center justify-around px-6 md:px-1 py-5">
-      <div className="flex flex-col items-start gap-6 text-center md:text-left">
+    <section className="w-full flex flex-col md:flex-row items-center justify-around md:px-1 p-5">
+      <div className="flex flex-col items-start gap-6 text-center">
         <div className="flex flex-col text-center w-full">
           <h3 className="text-5xl md:text-6xl font-bold text-white">Michał Pawłowski</h3>
           <h4 className="text-sm md:text-md font-bold text-white">MP.Dev_</h4>
-          <h5 className="text-2xl md:text-6xl font-bold text-white">
-            Junior Frontend Developer
-          </h5>
+          <h5 className="text-2xl font-bold text-white">Junior Frontend Developer</h5>
         </div>
         <p className="text-lg md:text-xl text-gray-200 max-w-xl">
-          Jestem Michał i rozwijam się w kierunku frontendu. Uczę się nowych technologii,
-          tworzę własne projekty i stale poszerzam swoje umiejętności.
+          Łączę doświadczenie w rozwiązywaniu problemów technicznych z obszaru IT Support
+          z praktyczną znajomością ekosystemu React. Moje zaplecze inżynierskie pozwala mi
+          dostarczać kod, który jest nie tylko funkcjonalny, ale również zoptymalizowany
+          pod kątem wydajności. Szukam wyzwań w projektach komercyjnych, w których mogę
+          rozwijać kompetencje frontendowe i współtworzyć nowoczesne, intuicyjne
+          interfejsy.
         </p>
         <div className="flex w-full justify-center items-center gap-3">
           <Button link={"/contact"} content="Kontakt ze mną" />
